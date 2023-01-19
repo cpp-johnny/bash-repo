d1830a81327a999f5d281ab535b5352b950f5e26
Johnny <119715263+cpp-johnny@users.noreply.github.com>
2023-01-19 13:08:24 +0800

    Create variables.sh

---
+++ variables.sh
@@ -0,0 +1,13 @@
+# to assign a variable, ensure there is no space between name and `=`
+
+# example:
+var1="This is my variable"
+
+# to display it, you can type:
+echo "$var1"
+# output: This is my variable
+
+# to delete(unset) the variable, you use `unset`
+
+# example:
+unset "$var1"
